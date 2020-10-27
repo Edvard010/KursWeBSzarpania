bdbc56c74d8e148b7739f762bb58bda1a65de602
Edvard010 <piszczatowski.grzegorz@gmail.com>
2020-10-27 20:41:38 +0100

    added method POST and button Wyślij

--- HelloWorld/Controllers/UserController.cs
+++ HelloWorld/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloWorld.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloWorld.Controllers
@@ -9,6 +10,11 @@ namespace HelloWorld.Controllers
     public class UserController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(EditUserViewModel data)
         {
             return View();
         }
